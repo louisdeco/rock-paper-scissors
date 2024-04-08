c04e7a87628ba176e6283fd8a3b64d48a963b48f
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-04-08 15:52:26 +0400

    Create function to select human choice
    Creation of a function to prompt the user to sele-
    ct an option between Rock, Paper and Scissors.

--- game.js
+++ game.js
@@ -1,11 +1,29 @@
 function getComputerChoice () {
     // The list of availale option
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     // Generate a random integer, either 0, 1, 2
-    let randomIndex = Math.floor(Math.random() * 3) 
+    let randomIndex = Math.floor(Math.random() * 3);
     // Index the array with the random number to have the computer choice
-    let computerChoice = choices[randomIndex]
+    let computerChoice = choices[randomIndex];
 
     // Return the choice of the computer
-    return computerChoice
+    return computerChoice;
+}
+
+function getHumanChoice () {
+    // Create a var to store the user's choice
+    let humanChoice = prompt("Chose your weapon between Rock, Paper and Scissors! Do not worry, the game is case-insensitive. Beware of spelling though")
+
+    // If user's choice correct, we return it
+    if (
+        humanChoice.toLowerCase() === "rock" ||
+        humanChoice.toLowerCase() === "paper" ||
+        humanChoice.toLowerCase() === "scissors"
+    ) {
+        return humanChoice
+    }
+    // If user's choice not correct, we display a message
+    else {
+        return console.log("Chose again.")
+    }
 }
