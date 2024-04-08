bdf553524ccb8bc5c44276dcf3b9a4b997fbcba9
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-04-08 15:31:31 +0400

    Create function for computer choice
    Creation of a function that select an option betw-
    een Rock, Paper and Scissors at random.

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+function getComputerChoice () {
+    // The list of availale option
+    let choices = ["Rock", "Paper", "Scissors"];
+    // Generate a random integer, either 0, 1, 2
+    let randomIndex = Math.floor(Math.random() * 3) 
+    // Index the array with the random number to have the computer choice
+    let computerChoice = choices[randomIndex]
+
+    // Return the choice of the computer
+    return computerChoice
+}
